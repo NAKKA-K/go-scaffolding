5adae7e51e274fd6f7607f10aec35db58dc2d988
NAKKA-K <nakkak618@gmail.com>
2024-12-03 16:23:00 +0900

    feat: コマンドの役割を変更した

---
+++ cmd/add.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "新しいアクションを追加する.",
+	Long: `[not implemented]
+	CreateやDeleteなど、2つ目以降のアクションを追加する際に使用するコマンドです.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("add called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
